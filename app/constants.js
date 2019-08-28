644c4e4d45c2ccbde3d88b9060aaa98f3e818086
Golan Levy <golan.levy@gong.io>
2019-08-28 19:13:19 +0300

    fix json punctuation

--- app/constants.js
+++ app/constants.js
@@ -66,3 +66,7 @@ export const ZOOM = 20;
 export const PANEL_HEIGHT = 300; //px
 
 export const EXTRA_DISCREPANCY_TIME = 1;
+
+export const PUNCTUATIONS = ". ! , … ?".split(' ');
+export const PUNCTUATION_TYPE = "PUNCTUATION"
+export const WORD_TYPE = "WORD"
--- app/controller.js
+++ app/controller.js
@@ -5,6 +5,7 @@ import * as constants from './constants'
 import './third-party/soundtouch.js'
 
 import {config} from './config.js'
+import {PUNCTUATION_TYPE} from "./constants";
 
 var Diff = require('diff')
 
@@ -1136,16 +1137,56 @@ class MainController {
         return ret;
     }
 
+    splitPunctuation(text) {
+        let punct = "";
+
+        while (constants.PUNCTUATIONS.indexOf(text[text.length - 1]) !== -1) {
+            punct = text[text.length - 1] + punct;
+            text = text.substring(0, text.length-1)
+        }
+
+        if(punct === '...'){
+            punct = '…';
+        }
+
+        return [text, punct];
+    }
+
     convertRegionsToJson(fileIndex) {
         var self = this;
         var data = {schemaVersion: "2.0", monologues: []};
         this.iterateRegions(function (region) {
+            let words = region.data.words;
+            let terms = []
+            if (words) {
+                words.forEach(w => {
+                    // copy word to cancel references
+                    let t = JSON.parse(JSON.stringify(w))
 
+                    t.type = constants.WORD_TYPE;
+                    terms.push(t);
+
+                    let textAndPunct = self.splitPunctuation(t.text);
+                    if (textAndPunct[1] !== "") {
+
+                        //trim the punctuation from the original
+                        t.text = textAndPunct[0]
+
+                        terms.push({
+                            start: t.end,
+                            end: t.end,
+                            text: textAndPunct[1],
+                            confidence: t.confidence,
+                            type: constants.PUNCTUATION_TYPE
+                        })
+                    }
+                })
+            }
             data.monologues.push({
                 speaker: {id: self.formatSpeaker(region.data.speaker), color: region.color},
                 start: region.start,
                 end: region.end,
-                terms: region.data.words
+                terms: terms
             });
 
         }, fileIndex, true);
@@ -1526,11 +1567,25 @@ class MainController {
             //     }
             // }
 
+
             if (monologue.terms) {
                 monologue.words = monologue.terms;
                 delete monologue.terms;
             } else {
                 monologue.words = [];
+            }
+
+
+            // attach punctuation to the previous word
+            for (let j = 1; j < monologue.words.length;) {
+                let current = monologue.words[j];
+
+                if (current.type === constants.PUNCTUATION_TYPE) {
+                    monologue.words[j - 1].text += current.text;
+                    monologue.words.splice(j, 1);
+                } else {
+                    j++;
+                }
             }
         }
         return monologues;
@@ -1549,7 +1604,7 @@ class MainController {
             let duration = parseFloat(cells[3]);
             let end = start + duration;
             let confidence = parseFloat(cells[5])
-            if (confidence === constants.NO_CONFIDENCE){
+            if (confidence === constants.NO_CONFIDENCE) {
                 confidence = undefined;
             }
 
