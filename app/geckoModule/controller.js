299be82cbc4505188cf730f5a6886b202d446994
Vit Rybn <strelok2010@gmail.com>
2020-01-24 16:31:07 +0800

    Fix error with loading CTM's in server mode

--- app/geckoModule/controller.js
+++ app/geckoModule/controller.js
@@ -72,7 +72,7 @@ class MainController {
         }
 
         const audio = urlParams.get('audio')
-        let formats = ['rttm', 'tsv', 'json', 'ctm']
+        let formats = ['rttm', 'tsv', 'json', 'ctm', 'srt']
         formats = formats.map((f) => {
             if (urlParams.get(f)) {
                 return {
@@ -1504,7 +1504,14 @@ class MainController {
             const urlArr = config.audio.url.split('/')
             const audioFileName = urlArr[urlArr.length - 1]
             self.audioFileName = audioFileName
-            res.segmentFiles.forEach(x => x.data = self.handleTextFormats(x.filename, x.data));
+            res.segmentFiles.forEach((x) => {
+                const data = self.handleTextFormats(x.filename, x.data)
+                x.data = Array.isArray(data) ? data[0] : data
+                const parsedColors = Array.isArray(data) && data.length > 1 ? data[1] : null
+                if (parsedColors) {
+                    self.fileSpeakerColors = parsedColors
+                }
+            });
             self.filesData = res.segmentFiles;
 
             if (config.enableDrafts && this.dataBase) {
