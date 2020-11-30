fc3650529999f229fb25b676fc7b52d079c200f2
rotem-1996 <rotem.eilaty@gong.io>
2020-11-30 15:32:27 +0200

    ocr mode filtering

--- app/geckoModule/directives/imageTable.js
+++ app/geckoModule/directives/imageTable.js
@@ -9,8 +9,35 @@ export const imageTableDirective = () => {
         templateUrl: imageTableTemplate,
         link: (scope, element, attrs) => {
 //            scope.imgSrc;
+            scope.parent.indexes = [];
+            scope.only_done = true;
             scope.clickPath = (index) =>{
                 scope.parent.imageOpen(index);
+            }
+
+            scope.changeSearch = () =>{
+                scope.parent.indexes = [];
+            }
+
+            scope.test = (imageLine,index)=>{
+                if(scope.search){
+                    if(imageLine.predicted_title == scope.search){
+                        scope.parent.indexes.push(index);
+                        return true;
+                    }
+                    return false;
+                }
+                else if(scope.only_done){
+                    if(imageLine.predicted_title != '' || !imageLine.presentation){
+                        return false;
+                    }
+                    scope.parent.indexes.push(index);
+                    return true;
+                }
+                else{
+                    scope.parent.indexes.push(index);
+                    return true;
+                }
             }
 
 //
